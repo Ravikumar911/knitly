a8ca670cf17cad26ea8b2d75820f258830326a31
Ravikumar911 <ravi.911kumar@gmail.com>
2025-04-06 14:18:40 +0530

    fix  build

--- apps/main/app/page.tsx
+++ apps/main/app/page.tsx
@@ -1,14 +1,11 @@
-import { HydrateClient, prefetch, trpc } from "@/trpc/server";
+import { HydrateClient } from "@/trpc/server";
 import { ClientGreeting } from "@/components/client-greeting";
 
-export default async function Home() {
-  prefetch(
-    trpc.hello.queryOptions({
-      text: 'server',
-    }),
-  );  
+// Adding export config for static rendering
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
 
-  
+export default function Home() {
   return (
     <HydrateClient>
       <ClientGreeting />
